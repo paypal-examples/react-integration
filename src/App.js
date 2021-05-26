751196411ee5663d98adbd3398b45796b45f6f51
Amy Egan <amyegan@paypal.com>
2021-05-25 21:32:57 -0400

    Use script tag instead of react-paypal-js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import logo from './logo.svg';
 import './App.css';
 import Checkout from './Checkout';
 
 function App() {
-  const CLIENT_ID = 'test';
-
   return (
-    <PayPalScriptProvider options={{ "client-id": CLIENT_ID }}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Buy an atom!
         </p>
-      <Checkout />
+        <Checkout />
       </header>
     </div>
-    </PayPalScriptProvider>
   );
 }
 
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,35 @@
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import React from "react";
+import ReactDOM from "react-dom"
 
-function Checkout() {
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+function Checkout(props) {
+  //let cartTotal = 0.02;
+
+  const createOrder = (data, actions) => {
+    console.log("Creating order...");
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: props.cartTotal || 0.02
+          },
+        },
+      ],
+    });
+  }
+
+  const onApprove = (data, actions) => {
+    console.log("Order approved...");
+    return actions.order.capture().then(alert("Order complete!"));
+  }
+
   return (
     <div style={{ width: "75%", maxWidth: "500px" }}>
-      <PayPalButtons 
-        style={{ layout: "horizontal", tagline: "false" }} />
+      <PayPalButton
+        style={{ layout: "horizontal", tagline: "false" }} 
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)} />
     </div>
   );
 }
