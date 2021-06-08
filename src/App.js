844ee141ea0378809bf52223f2657f5b68e40f1b
AnuragVathalurin <anuragvathalurin@gmail.com>
2021-06-08 15:31:27 -0400

    Spinner and Currency Change Implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import Checkout from './Checkout';
@@ -6,15 +7,21 @@ import Checkout from './Checkout';
 function App() {
   const CLIENT_ID = 'test';
 
+  const [DeferLoadingProp, setDeferLoadingProp] = useState(true);
+
+  const deferLoadingHandler = (loading) => {
+    setDeferLoadingProp(loading);
+  }
+
   return (
-    <PayPalScriptProvider options={{ "client-id": CLIENT_ID }}>
+    <PayPalScriptProvider deferLoading={DeferLoadingProp} options={{ "client-id": CLIENT_ID }}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Buy an atom!
         </p>
-      <Checkout />
+      <Checkout onDeferLoading={deferLoadingHandler}/>
       </header>
     </div>
     </PayPalScriptProvider>
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,40 @@
-import { PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import { useState } from "react";
+import loading from './three-dots.svg';
 
-function Checkout() {
+function Checkout(props) {
+
+  const [{ options, isPending,  isInitial}, dispatch] = usePayPalScriptReducer();
+  const [currency, setCurrency] = useState(options.currency);
+
+  const saveDeferLoadingHandler = () => {
+    props.onDeferLoading(false);
+  }
+
+
+  function onCurrencyChange({ target: { value }}) {
+    setCurrency(value);
+    dispatch({
+      type: "resetOptions",
+      value: {
+        ...options,
+        currency: value,
+      },
+    });
+  }
+
   return (
     <div style={{ width: "75%", maxWidth: "500px" }}>
+      {isInitial ? <button onClick={saveDeferLoadingHandler}>Load JS SDK</button> : null}
+
+      {isPending ? <img src={loading} /> : null}
       <PayPalButtons 
         style={{ layout: "horizontal", tagline: "false" }} />
+
+      <select value={currency} onChange={onCurrencyChange}>
+            <option value="USD">United States dollar</option>
+            <option value="EUR">Euro</option>
+        </select>  
     </div>
   );
 }
