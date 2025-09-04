991118955ca0fbbf79d034c25e66196c07bf82dd
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-09-04 09:15:14 -0500

    update react app env declaration file

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -11,33 +11,3 @@ declare module '*.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-
-declare module '*.module.css' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module '*.png' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.jpg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.jpeg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.gif' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.webp' {
-  const value: string;
-  export default value;
-}
